96f4304c4b26184df78c15425a6204b56bb0d52b
Rudra Patel <102151482+IamRudraPatel@users.noreply.github.com>
2024-07-11 13:11:06 +0530

    Time: 42 ms (16.58%) | Memory: 16.4 MB (81.94%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.py
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.py
@@ -1,31 +1,17 @@
 class Solution:
-    def isBracket(self, ch) -> bool:
-        if ch=='(' or ch==')': return True
-        return False
-    
-    def reverseRange(self, st, low, high) -> None:
-        while (low <= high):
-            if (not self.isBracket(st[low])) and (not self.isBracket(st[high])):
-                st[low], st[high] = st[high], st[low]
-                low += 1
-                high -= 1
-                continue
-            if self.isBracket(st[low]):
-                low += 1
-            if self.isBracket(st[high]):
-                high -= 1
+    def reverse(self, arr, low, high):
+        while low<=high:
+            arr[low], arr[high] = arr[high], arr[low]
+            low += 1 
+            high -= 1
 
     def reverseParentheses(self, s: str) -> str:
-        st = list(s)
-        stack = []
-        for i in range(len(st)):
-            if st[i]=='(':
-                stack.append(i)
-            elif st[i]==')':
-                self.reverseRange(st, stack.pop()+1, i-1)
-            else: continue
-        ans = ""
-        for ch in st:
-            if not self.isBracket(ch):
-                ans += ch
-        return ans
+        answer = []
+        skipLen = []
+        for ch in s:
+            if ch == '(':
+                skipLen.append(len(answer))
+            elif ch == ')':
+                self.reverse(answer, skipLen.pop(), len(answer)-1)
+            else: answer.append(ch)
+        return "".join(answer)
