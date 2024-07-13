2b16ff4344b94bdb359ba6b06eb8205122da068d
Rudra Patel <102151482+IamRudraPatel@users.noreply.github.com>
2024-07-13 13:30:27 +0530

    Time: 974 ms (62.50%) | Memory: 41.3 MB (67.31%) - LeetSync

--- 2846-robot-collisions/robot-collisions.py
+++ 2846-robot-collisions/robot-collisions.py
@@ -1,40 +1,25 @@
 class Solution:
-    def survivedRobotsHealths(
-        self, positions: List[int], healths: List[int], directions: str
-    ) -> List[int]:
+    def survivedRobotsHealths(self, positions, healths, directions):
         n = len(positions)
-        indices = list(range(n))
-        result = []
-        stack = deque()
-        # Sort indices based on their positions
-        indices.sort(key=lambda x: positions[x])
-
-        for current_index in indices:
-            # Add right-moving robots to the stack
-            if directions[current_index] == "R":
-                stack.append(current_index)
-            else:
-                while stack and healths[current_index] > 0:
-                    # Pop the top robot from the stack for collision check
-                    top_index = stack.pop()
-
-                    if healths[top_index] > healths[current_index]:
-                        # Top robot survives, current robot is destroyed
-                        healths[top_index] -= 1
-                        healths[current_index] = 0
-                        stack.append(top_index)
-                    elif healths[top_index] < healths[current_index]:
-                        # Current robot survives, top robot is destroyed
-                        healths[current_index] -= 1
-                        healths[top_index] = 0
-                    else:
-                        # Both robots are destroyed
-                        healths[current_index] = 0
-                        healths[top_index] = 0
-
-        # Collect surviving robots
-        for index in range(n):
-            if healths[index] > 0:
-                result.append(healths[index])
-
-        return result
+        index = {robot: i for i, robot in enumerate(positions)}
+        positions.sort()
+        recentRobot = []
+        for robot in positions:
+            i = index[robot]
+            if directions[i]=='R':
+                recentRobot.append(i)
+            else: 
+                while (recentRobot) and (healths[i]!=0) and (directions[recentRobot[-1]]=='R'):
+                    health1, health2 = healths[recentRobot[-1]], healths[i]
+                    if health1 == health2:
+                        healths[recentRobot.pop()] = 0
+                        healths[i] = 0
+                    elif health1 > health2:
+                        healths[recentRobot[-1]] -= 1
+                        healths[i] = 0
+                    else: 
+                        healths[recentRobot.pop()] = 0
+                        healths[i] -= 1
+                if healths[i]: 
+                    recentRobot.append(i)
+        return [h for h in healths if h>0]
