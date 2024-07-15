895f9d629ae6464c76cb68531beb628ccff8eb73
Rudra Patel <102151482+IamRudraPatel@users.noreply.github.com>
2024-07-15 12:06:44 +0530

    Time: 38 ms (42.86%) | Memory: 16.7 MB (28.81%) - LeetSync

--- 726-number-of-atoms/number-of-atoms.py
+++ 726-number-of-atoms/number-of-atoms.py
@@ -2,38 +2,37 @@ class Solution:
     def countOfAtoms(self, formula: str) -> str:
         n: int = len(formula)
         i: int = 0
-        stack: list[{str, int}] = [defaultdict(int)]
+        stack: list = [defaultdict(int)]
         while i < n:
-            curCh: str = formula[i]
-            if curCh.isupper():
-                element: str = curCh
+            curCh = formula[i]
+            if curCh == '(':
+                stack.append(defaultdict(int))
+                i+=1
+            elif curCh == ')':
+                num = ""
                 i += 1
-                if (i < n) and (formula[i].islower()):
-                    element += formula[i]
+                while (i<n) and (formula[i].isdigit()):
+                    num += formula[i]
                     i += 1
-                atomCount: str = ""
-                while (i < n) and (formula[i].isdigit()):
+                num = 1 if not num else int(num)
+                curMap = stack.pop()
+                prevMap = stack[-1]
+                for eles in curMap:
+                    prevMap[eles] += curMap[eles] * num
+            elif curCh.isupper(): 
+                element = curCh
+                atomCount = ""
+                i += 1
+                if (i<n) and (formula[i].islower()):
+                    element += formula[i]
+                    i += 1 
+                while (i<n) and (formula[i].isdigit()):
                     atomCount += formula[i]
                     i += 1
                 atomCount = 1 if not atomCount else int(atomCount)
                 stack[-1][element] += atomCount
-            elif curCh == "(":
-                stack.append(defaultdict(int))
-                i += 1
-            elif curCh == ")":
-                num: str = ""
-                i += 1
-                while (i < n) and (formula[i].isdigit()):
-                    num += formula[i]
-                    i += 1
-                num = 1 if not num else int(num)
-                curMap: defaultdict[str, int] = stack.pop()
-                prevMap: defaultdict[str, int] = stack[-1]
-                for eles in curMap:
-                    prevMap[eles] += curMap[eles] * num
-            else: i += 1
-        answer: str = ""
-        finalMap: defaultdict[str, int] = stack.pop()
+        answer = ""
+        finalMap = stack.pop()
         for eles in sorted(finalMap.keys()):
             atomCount = finalMap[eles]
             answer += eles if atomCount == 1 else eles + str(atomCount)
