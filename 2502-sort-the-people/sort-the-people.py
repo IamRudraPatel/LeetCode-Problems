2d8e46c04a160ac1c9dfb0bb73c64eb9e7484fde
Rudra Patel <102151482+IamRudraPatel@users.noreply.github.com>
2024-07-22 11:48:18 +0530

    Time: 106 ms (52.59%) | Memory: 17 MB (84.16%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -2,8 +2,8 @@ class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         nameMap = {}
         for i in range(len(heights)):
-            nameMap[heights[i]] = names[i]
+            nameMap[heights[i]] = i
         ans = []
         for height in sorted(heights, reverse=True):
-            ans.append(nameMap[height])
+            ans.append(names[nameMap[height]])
         return ans
