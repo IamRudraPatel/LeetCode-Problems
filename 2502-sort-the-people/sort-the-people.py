c7fdab6383304e683a4cbb01f230d015ad43861c
Rudra Patel <102151482+IamRudraPatel@users.noreply.github.com>
2024-07-22 12:12:09 +0530

    Time: 367 ms (8.70%) | Memory: 17.2 MB (14.55%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,9 +1,31 @@
 class Solution:
-    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        nameMap = {}
-        for i in range(len(heights)):
-            nameMap[heights[i]] = i
-        ans = []
-        for height in sorted(heights, reverse=True):
-            ans.append(names[nameMap[height]])
-        return ans
+    def InPlaceMerge(self, heights, names, start, mid, end):
+        l: int = start
+        r: int = mid + 1
+        while l <= mid and r <= end:
+            if heights[l] >= heights[r]:
+                l = l + 1
+            else:
+                temp1: int = heights[r]
+                temp2: str = names[r]
+                idx: int = r
+                while idx > l:
+                    heights[idx] = heights[idx - 1]
+                    names[idx] = names[idx - 1]
+                    idx -= 1
+                heights[l] = temp1
+                names[l] = temp2
+                l += 1
+                r += 1
+                mid += 1
+            
+    def mergeSort(self, heights, names, start, end):
+        if start>=end: return;
+        mid: int = (start+end)//2
+        self.mergeSort(heights, names, start, mid)
+        self.mergeSort(heights, names, mid+1, end)
+        self.InPlaceMerge(heights, names, start, mid, end)
+
+    def sortPeople(self, names, heights ) -> list[str]:
+        self.mergeSort(heights, names, 0, len(heights)-1)
+        return names
