7b620b35ff3ca8d77e03e4fb26d636542c841446
Rudra Patel <102151482+IamRudraPatel@users.noreply.github.com>
2024-07-24 12:25:41 +0530

    Time: 870 ms (88.59%) | Memory: 24.8 MB (47.83%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
@@ -3,14 +3,14 @@ class Solution:
         mappedNums = []
         for i, num in enumerate(nums):
             newNum = 0
-            base = 1
-            if num == 0: newNum = mapping[0]
-            else: 
-                while num:
-                    digit = num % 10
-                    num = num // 10
-                    newNum += base * mapping[digit]
-                    base *= 10
+            place = 1
+            if num == 0: 
+                mappedNums.append((mapping[0], i))
+                continue
+            while num:
+                newNum += place * mapping[num % 10]
+                num = num // 10
+                place *= 10
             mappedNums.append((newNum, i)) # (mappedNum, index)
         mappedNums.sort()
         return [nums[pair[1]] for pair in mappedNums]         
