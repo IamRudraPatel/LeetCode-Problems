c47c16c3e1d888eba4916d57b71d4e7c89929592
Rudra Patel <102151482+IamRudraPatel@users.noreply.github.com>
2024-07-30 12:43:29 +0530

    Time: 169 ms (91.00%) | Memory: 17.5 MB (92.33%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.py
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.py
@@ -1,11 +1,9 @@
-from collections import deque
 class Solution:
     def minimumDeletions(self, s: str) -> int:
-        st1 = deque()
-        ans1: int = 0
+        ans, bCount = 0, 0
         for ch in s:
-            if (ch == 'a') and (st1) and (st1[-1]=='b'):
-                ans1 += 1
-                st1.pop()
-            else:st1.append(ch)
-        return ans1
+            if ch == 'b': bCount += 1
+            elif bCount:
+                ans += 1
+                bCount -= 1
+        return ans
