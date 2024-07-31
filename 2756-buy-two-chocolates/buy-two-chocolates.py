7a1e65e9b755bc2b9450e9240ecd1fa240f1d5cc
Rudra Patel <102151482+IamRudraPatel@users.noreply.github.com>
2024-07-31 13:03:39 +0530

    Time: 62 ms (38.17%) | Memory: 16.5 MB (28.52%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.py
+++ 2756-buy-two-chocolates/buy-two-chocolates.py
@@ -1,9 +1,6 @@
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
         prices.sort()
-        amount, bought = money, 0
-        for price in prices:
-            if (price <= amount) and (bought!=2):
-                amount -= price
-                bought += 1
-        return amount if bought == 2 else money
+        summ = prices[0]+prices[1]
+        return money if summ > money else money - summ
+    
