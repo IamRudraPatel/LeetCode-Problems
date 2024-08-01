99d97b9914101c4e84ba8d6dc34473a0208de8d0
Rudra Patel <102151482+IamRudraPatel@users.noreply.github.com>
2024-08-01 10:41:28 +0530

    Time: 42 ms (83.17%) | Memory: 16.3 MB (97.00%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.py
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.py
@@ -2,9 +2,5 @@ class Solution:
     def countSeniors(self, details: List[str]) -> int:
         ans = 0
         for d in details:
-            tens = ord(d[11]) - ord('0')
-            ones = ord(d[12]) - ord('0')
-            age = 10 * tens + ones
-            if age > 60:
-                ans += 1
+            if int(d[11: 13]) > 60: ans += 1
         return ans
