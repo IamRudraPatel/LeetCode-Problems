2cc0680b8d7db3b4946ec1c90c0c4c7f44048ac0
Rudra Patel <102151482+IamRudraPatel@users.noreply.github.com>
2024-08-03 10:27:04 +0530

    Time: 72 ms (70.58%) | Memory: 16.8 MB (22.18%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.py
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.py
@@ -1,9 +1,3 @@
 class Solution:
     def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
-        count = defaultdict(int)
-        for i in range(len(target)):
-            count[target[i]] += 1
-            count[arr[i]] -= 1
-        for n in count:
-            if count[n] != 0: return False
-        return True
+        return sorted(target) == sorted(arr)
